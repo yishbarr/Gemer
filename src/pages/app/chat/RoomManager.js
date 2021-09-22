0e6171aae62fb68dbc4a46fe7ccc030d3edced01
Yishai Barr <yishbarr@gmail.com>
2021-09-22 10:16:22 +0300

    update

--- src/pages/app/chat/RoomManager.js
+++ src/pages/app/chat/RoomManager.js
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 import { useContext, useEffect, useRef, useState } from "react";
-import { Button, Container, Form, Modal, Alert } from "react-bootstrap";
+import { Button, Container, Form, Modal, Alert, Table } from "react-bootstrap";
 import { Redirect, useParams } from "react-router-dom";
 import { fieldsClass } from "../../../constants/Classes";
 import { Context } from "../../../context/Store";
@@ -14,7 +14,7 @@ export default function RoomManager(p) {
     const managerRef = roomRef.child("managers");
     const usersRef = database.ref("users");
     //States
-    const [room, setRoom] = useState({ });
+    const [room, setRoom] = useState({});
     const [description, setDescription] = useState();
     const [game, setGame] = useState();
     const [isValidRoom, setIsValidRoom] = useState(true);
@@ -87,42 +87,59 @@ export default function RoomManager(p) {
     }
     if (deleted)
         return <Redirect to="/app/myRooms" />
-    console.log(managers);
+    console.log(owners);
     return (
         <Container>
             <h1>{room} Settings</h1>
             <Form.Group className="mb-3">
                 <Form.Label>Room name</Form.Label>
-                <Form.Control className={fieldsClass} value={room} onChange={e => setRoom(e.target.value)} disabled={!isManager.current} />
+                <Form.Control className={fieldsClass} value={room} onChange={e => setRoom(e.target.value)} disabled={!isOwner.current} />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Room description</Form.Label>
-                <Form.Control className={fieldsClass} value={description} onChange={e => setDescription(e.target.value)} disabled={!isManager.current} />
+                <Form.Control className={fieldsClass} value={description} onChange={e => setDescription(e.target.value)} disabled={!isOwner.current} />
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Game</Form.Label>
-                <Form.Control className={fieldsClass} value={game} onChange={e => setGame(e.target.value)} disabled={!isManager.current} />
+                <Form.Control className={fieldsClass} value={game} onChange={e => setGame(e.target.value)} disabled={!isOwner.current} />
             </Form.Group>
+            {isOwner.current ?
+                <Form.Group className="mb-3">
+                    <Button variant="success" >Apply</Button>
+                </Form.Group> : null}
             <Form.Group className="mb-3">
-                <Button variant="success" disabled={!isManager.current}>Apply</Button>
-            </Form.Group>
-            <Form.Group className="mb-3">
-                <Button variant="danger" disabled={isManager.current && managers.length === 1} onClick={leaveRoom}>Leave Room</Button>
+                <Button variant="danger" disabled={isOwner.current && owners.length === 1} onClick={leaveRoom}>Leave Room</Button>
             </Form.Group>
             <Form.Label>Managers</Form.Label>
-            <Form.Group className="mb-3">
-                <Form.Label>Add Manager</Form.Label>
-                <Form.Control id="addManagerField" className={fieldsClass + " mb-3"} placeholder="Type in user ID" disabled={!isManager.current} />
-                <Button variant="info" onClick={addManager} disabled={!isManager.current}>Add</Button>
-            </Form.Group>
+            {isOwner.current ?
+                <Form.Group className="mb-3">
+                    <Form.Label>Add Manager</Form.Label>
+                    <Form.Control id="addManagerField" className={fieldsClass + " mb-3"} placeholder="Type in user ID" disabled={!isOwner.current} />
+                    <Button variant="info" onClick={addManager}>Add</Button>
+                </Form.Group> : null}
             <Alert variant="primary" show={showManagerNote.length > 0}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                 </svg>
                 {showManagerNote}
             </Alert>
-            {managers.map(m => <p>{m.name} - {m.key}</p>)}
-            <Button variant="danger" onClick={() => setShowDelete(true)} disabled={!isManager.current}>Delete Room</Button>
+            <Table variant="dark">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>User ID</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {managers.map(m => <tr>
+                        <td>{m.name}</td>
+                        <td>{m.key}</td>
+                        {!owners.includes(m) ? <td><Button variant="danger" onClick={() => managerRef.child(m.key).remove()}>Remove Manager</Button></td> : <td />}
+                    </tr>)}
+                </tbody>
+            </Table>
+            {isOwner.current ? <Button variant="danger" onClick={() => setShowDelete(true)} >Delete Room</Button> : null}
             <Modal show={showDelete} onHide={hideDelete} >
                 <Modal.Header closeButton>
                     <Modal.Title>Delete Room</Modal.Title>
